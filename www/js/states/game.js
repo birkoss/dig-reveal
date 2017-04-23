4b903f90bb3033d86733cba9a88a7eb22d182463
birkoss <admin@birkoss.com>
2017-04-22 21:51:10 -0400

    Starting a panel

--- www/js/states/game.js
+++ www/js/states/game.js
@@ -5,11 +5,17 @@ GAME.Game = function() {};
 GAME.Game.prototype = {
     create: function() {
         this.mapContainer = this.game.add.group();
+        this.panelContainer = this.game.add.group();
 
         this.createMap();
-    },
+        this.createPanel();
 
-    createMap() {
+        this.showPanel();
+    },
+    showPanel: function() {
+        this.game.add.tween(this.panelContainer).to({y:0}, 1000, Phaser.Easing.Bounce.Out).start();
+    },
+    createMap: function() {
         /* Get the best map size for the current resolution */
         let mapWidth = Math.floor(this.game.width / (16 * GAME.RATIO));
 
@@ -26,5 +32,35 @@ GAME.Game.prototype = {
 
         this.map.x = (this.game.width - this.map.width)/2;
         this.map.y = 32;
+    },
+    createPanel: function() {
+        let background = this.panelContainer.create(0, 0, 'panel:background');
+        background.scale.setTo(GAME.RATIO, GAME.RATIO);
+        background.width = this.game.width;
+
+        let mapName = this.game.add.bitmapText(16, (background.height/2), 'font:gui', 'Village', 16);
+        mapName.anchor.set(0, 0.5);
+        this.panelContainer.addChild(mapName);
+
+        let staminaIcon = this.panelContainer.create(0, (background.height/2), 'panel:stamina');
+        staminaIcon.anchor.set(1, 0.5);
+        staminaIcon.scale.setTo(GAME.RATIO, GAME.RATIO);
+        staminaIcon.x = background.width - 16;
+
+        let progressBarBackground = this.panelContainer.create(0, (background.height/2), 'progress-bar:background');
+        progressBarBackground.anchor.set(0, 0.5);
+        progressBarBackground.scale.setTo(GAME.RATIO, GAME.RATIO);
+        progressBarBackground.x = staminaIcon.x - progressBarBackground.width - staminaIcon.width - 16;
+
+        this.stamina = this.panelContainer.create(progressBarBackground.x, (background.height/2), 'progress-bar:filling');
+        this.stamina.anchor.set(0, 0.5);
+        this.stamina.scale.setTo(GAME.RATIO, GAME.RATIO);
+        this.stamina.originalWidth = this.stamina.width;
+
+        this.staminaText = this.game.add.bitmapText(this.stamina.x + (this.stamina.width/2), (background.height/2), 'font:gui', '100/100', 8);
+        this.staminaText.anchor.set(0.5, 0.5);
+        this.panelContainer.addChild(this.staminaText);
+
+        this.panelContainer.y = - this.panelContainer.height;
     }
 };
--- www/js/states/preload.js
+++ www/js/states/preload.js
@@ -4,7 +4,7 @@ GAME.Preload = function() {};
 
 GAME.Preload.prototype = {
     preload: function() {
-        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 128, 'gui:preloader');
+        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'gui:preloader');
         this.preloadBar.anchor.set(0.5);
         this.load.setPreloadSprite(this.preloadBar);
 
@@ -24,6 +24,14 @@ GAME.Preload.prototype = {
         this.load.image('tile:water-top-right', 'images/tiles/water/top-right.png');
         this.load.image('tile:water-bottom-left', 'images/tiles/water/bottom-left.png');
         this.load.image('tile:water-bottom-right', 'images/tiles/water/bottom-right.png');
+
+        this.load.image('panel:background', 'images/panel/background.png');
+        this.load.image('panel:stamina', 'images/panel/stamina.png');
+
+        this.load.image('progress-bar:background', 'images/progress-bar/background.png');
+        this.load.image('progress-bar:filling', 'images/progress-bar/filling.png');
+
+        this.load.bitmapFont('font:gui', 'images/fonts/gui.png', 'images/fonts/gui.xml');
     },
     create: function() {
         this.state.start('Game');
